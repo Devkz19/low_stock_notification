4f10237716259dbf7afa9b1f160e70cdbe9a23e7
Dev Zinzuvadiya <devkzinzuvadiya19@gmail.com>
2025-11-06 21:28:10 +0530

    Removed all Python files

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import res_company
-from . import res_users
-from . import res_config_settings
-from . import stock_notification
-from . import report_low_stock
-from . import product_individual_temp
--- models/res_users.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class ResUsers(models.Model):
-    _inherit = 'res.users'
-
-    notify_user = fields.Boolean(
-        string="Notify User",
-        help="Enable to receive notifications about low stock or system alerts."
-    )
